81f3c4271fffcd2a558c23c895861fe0b1297f75
Sadekur Rahaman <biploburrahaman@gmail.com>
2019-09-10 00:50:07 +0600

    Create uri-1008.cpp

---
+++ uri-1008.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int main()
+{
+    int wh, n;
+    float aph;
+
+    cin>>n>>wh>>aph;
+    printf("NUMBER = %d\nSALARY = U$ %.2f\n", n, wh*aph);
+
+    return 0;
+}
