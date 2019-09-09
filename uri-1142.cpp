60b2fee8287791f8eebf74e5646f34edbdfa9fc4
Sadekur Rahaman <biploburrahaman@gmail.com>
2019-09-10 01:43:28 +0600

    Create uri-1142.cpp

---
+++ uri-1142.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, i, val=0, j;
+    cin>>n;
+    for(i=0;i<n;i++){
+
+        for(j=0;j<3;j++){
+            val++;
+            cout<<val<<" ";
+        }
+    cout<<"PUM"<<endl;
+    val++;
+    }
+
+    return 0;
+}
