b990ced5edaeda63834bf3876861eba35552cf83
Sadekur Rahaman <biploburrahaman@gmail.com>
2019-09-10 00:49:31 +0600

    Create uri-1007.cpp

---
+++ uri-1007.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+
+    cin>>a>>b>>c>>d;
+
+    printf("DIFERENCA = %d\n", (a*b - c*d));
+
+    return 0;
+}
