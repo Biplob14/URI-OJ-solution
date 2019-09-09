b62d82a60fe891996d36b30dceff5b1c0aee8c65
Sadekur Rahaman <biploburrahaman@gmail.com>
2019-09-10 00:51:32 +0600

    Create uri-1011.cpp

---
+++ uri-1011.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdio>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    float r;
+    cin>>r;
+
+    printf("VOLUME = %.3f\n", (4.0/3)*3.14159*pow(r, 3));
+
+    return 0;
+}
