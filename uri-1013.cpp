d2073058a42363c92a841da498c34fba00d8d2c9
Sadekur Rahaman <biploburrahaman@gmail.com>
2019-09-10 00:52:26 +0600

    Create uri-1013.cpp

---
+++ uri-1013.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    cin>>a>>b>>c;
+
+    if(a>b && a>c){
+        printf("%d eh o maior\n", a);
+    }
+    else if(b>a && b>c){
+        printf("%d eh o maior\n", b);
+    }
+    else
+        printf("%d eh o maior\n", c);
+
+    return 0;
+}
