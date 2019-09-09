187b94ac9906dcce556336c3c5a93194dbc6fc4b
Sadekur Rahaman <biploburrahaman@gmail.com>
2019-09-10 00:54:13 +0600

    Create uri-1016.cpp

---
+++ uri-1016.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int main()
+{
+    int x, y, distance;
+
+    cin>>distance;
+
+    printf("%d minutos\n", distance*2);
+
+    return 0;
+}
