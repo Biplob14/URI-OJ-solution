39cc4870ea91d06909741db8d347c0c5b33f153d
Sadekur Rahaman <biploburrahaman@gmail.com>
2019-10-06 02:40:00 +0600

    Create uri-1172.cpp

---
+++ uri-1172.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    int x[10], i;
+
+    for(i=0;i<10;i++){
+        cin>>x[i];
+
+        if(x[i]<=0){
+            cout<<"X["<<i<<"] = 1"<<endl;
+        }
+        else
+            cout<<"X["<<i<<"] = "<<x[i]<<endl;
+    }
+
+    return 0;
+}
