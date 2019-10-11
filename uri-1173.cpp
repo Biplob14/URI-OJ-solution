1bb811a481756d5bed0773da51fd1aaa34f78ab8
Sadekur Rahaman <biploburrahaman@gmail.com>
2019-10-12 00:40:50 +0600

    Create uri-1173.cpp

---
+++ uri-1173.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int v, i, j, arr;
+    cin>>v;
+    for(i=0;i<10;i++){
+        cout<<"N["<<i<<"] = "<<v<<endl;
+         v = v*2;
+    }
+
+    return 0;
+}
