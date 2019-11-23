f9657c66e2574609e1d0ddc5d5fd811e2a2737dc
Sadekur Rahaman <biploburrahaman@gmail.com>
2019-11-23 22:28:14 +0600

    Create uri-1182.cpp

---
+++ uri-1182.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    int l, i, j;
+    char c;
+    float n[12][12], sum=0;
+    cin>>l>>c;
+    for(i=0;i<12;i++){
+        for(j=0;j<12;j++){
+            cin>>n[i][j];
+        }
+    }
+    for(i=0;i<12;i++){
+        sum += n[i][l];
+    }
+    if(c=='M') cout<<fixed<<setprecision(1)<<sum/12<<endl;
+    else
+        cout<<fixed<<setprecision(1)<<sum<<endl;
+
+
+    return 0;
+}
+//1 2 -3 9 -5 5 6 1 -12 -10 11 12
