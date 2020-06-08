acc777ab88fda7267f01d8e43c3e937a670b9b55
Sadekur Rahaman <biploburrahaman@gmail.com>
2020-06-09 00:51:50 +0600

    Create uri-1028_loop_solution.cpp
    https://www.urionlinejudge.com.br/judge/en/problems/view/1028

---
+++ uri-1028_loop_solution.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int gcd_naive(int a, int b) {
+  int current_gcd = 1;
+  for (int d = 2; d <= a && d <= b; d++) {
+    if (a % d == 0 && b % d == 0) {
+      if (d > current_gcd) {
+        current_gcd = d;
+      }
+    }
+  }
+  return current_gcd;
+}
+
+int gcd_fast(int a, int b) {
+    int divisor = a >= b ? a : b;
+    int dividend = a <= b ? a : b;
+    while (divisor != 0) {
+        int remainder = dividend % divisor;
+        dividend = divisor;
+        divisor = remainder;
+    }
+    return dividend;
+}
+
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+  //std::cout << gcd_naive(a, b) << std::endl;
+  std::cout << gcd_fast(a, b) << std::endl;
+  return 0;
+}
